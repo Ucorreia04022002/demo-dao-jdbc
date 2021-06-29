3a9ade7bd09d3c24eb03aafe33914eaf3167b3a6
Ucorreia04022002 <ulissescoquettocorreia@gmail.com>
2021-06-29 00:00:33 -0300

    DepartmentDAO insert implemented

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,97 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements  DepartmentDao{
+
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Department obj) {
+		PreparedStatement st =  null;
+		try {
+			st =  conn.prepareStatement(
+					"INSERT INTO department "
+					+ "(Id, Name) "
+					+ "VALUES "
+					+ "(?, ?) ");
+			st.setInt(1, obj.getId());
+			st.setString(2, obj.getName());
+			
+			Set<Department> set = new HashSet<Department>();
+			
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+				}
+				while (rs.next()) {
+					Integer ide = rs.getInt(1);
+					String name = rs.getString(2);
+					set.add(new Department(ide, name));
+				}
+				for (Department depi : set) {
+					if (depi.getId() == rs.getInt(1)) {
+						throw new DbException("This department id already exist!!");
+					}
+				}
+				
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! No rows Affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
