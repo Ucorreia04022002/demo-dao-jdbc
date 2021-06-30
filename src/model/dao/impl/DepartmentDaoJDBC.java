e9645c2285552527624f032bd59b2464d1e9ffb1
Ucorreia04022002 <ulissescoquettocorreia@gmail.com>
2021-06-29 22:00:07 -0300

    Finish

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -25,36 +25,33 @@ public class DepartmentDaoJDBC implements  DepartmentDao{
 	
 	@Override
 	public void insert(Department obj) {
-		PreparedStatement st =  null;
+		PreparedStatement st = null;
 		try {
-			st =  conn.prepareStatement(
-					"INSERT INTO department "
-					+ "(Id, Name) "
-					+ "VALUES "
-					+ "(?, ?) ",
-					Statement.RETURN_GENERATED_KEYS);
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getName());
-			
-			Set<Department> set = new HashSet<Department>();
-			
+			st = conn.prepareStatement(
+				"INSERT INTO department " +
+				"(Name) " +
+				"VALUES " +
+				"(?)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getName());
+
 			int rowsAffected = st.executeUpdate();
+
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
-				
-				DB.closeResultSet(rs);
 			}
 			else {
-				throw new DbException("Unexpected error! No rows Affected!");
+				throw new DbException("Unexpected error! No rows affected!");
 			}
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
+		} 
 		finally {
 			DB.closeStatement(st);
 		}
